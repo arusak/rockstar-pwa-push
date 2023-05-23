54d4828cff31a08cee4781425131448b6f3f07b4
Anton Rusak <anton.rusak@perfectart.com>
2023-05-23 18:11:41 +0200

    fix ssl keys

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import pathsPlugin from 'vite-tsconfig-paths';
 import checkerPlugin from 'vite-plugin-checker';
@@ -7,7 +7,8 @@ import { readFileSync } from 'fs';
 import { cssModulesDevClassNames } from './vite-css-modules-dev-classnames';
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  const { SSL_PRIVATE_KEY_PATH, SSL_PUBLIC_KEY_PATH } = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [
       react(),
@@ -36,8 +37,8 @@ export default defineConfig(() => {
     server: {
       port: 3000,
       https: {
-        key: readFileSync('c:/users/captain/.openssl/localhost.key'),
-        cert: readFileSync('c:/users/captain/.openssl/localhost.crt'),
+        key: SSL_PRIVATE_KEY_PATH && readFileSync(SSL_PRIVATE_KEY_PATH),
+        cert: SSL_PUBLIC_KEY_PATH && readFileSync(SSL_PUBLIC_KEY_PATH),
       }
     },
     build: {
