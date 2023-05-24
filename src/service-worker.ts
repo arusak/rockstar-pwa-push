0e2874150e48185d6a351ed7fb57bdcbd06ae0e7
Anton Rusak <anton.rusak@perfectart.com>
2023-05-24 14:27:45 +0200

    refactor sw

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -2,7 +2,6 @@
 
 import { precacheAndRoute } from 'workbox-precaching';
 import { clientsClaim } from 'workbox-core';
-import { registerPushSubscription, sendLog, showNotification, requestPush, showBadge } from './service-worker.utils';
 
 declare global {
   interface WorkerNavigator {
@@ -12,6 +11,76 @@ declare global {
 }
 
 declare const self: ServiceWorkerGlobalScope;
+
+/** METHODS **/
+
+export const sendLog = async (message: string) => {
+  console.info(message);
+  const clients = await self.clients.matchAll({ includeUncontrolled: true, type: 'window' });
+
+  clients.forEach(client => {
+    client.postMessage({
+      type: 'LOG',
+      message
+    });
+  });
+};
+
+export const showBadge = (count: number): Promise<void> => {
+  if ('setAppBadge' in self.navigator) {
+    return self.navigator.setAppBadge(count);
+  }
+  sendLog('Badge API is not available');
+  return Promise.resolve();
+};
+
+export const registerPushSubscription = async (): Promise<PushSubscription | null> => {
+  if ('pushManager' in self.registration && typeof self.registration.pushManager.subscribe === 'function') {
+    await sendLog('Registering a push subscription');
+    try {
+      const subscription = await self.registration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: import.meta.env.VITE_VAPID_KEY
+      });
+      await sendLog(`Subscribed successfully`);
+      return subscription;
+    } catch (e) {
+      await sendLog(`Unable to subscribe. ${e}`);
+    }
+  }
+  return null;
+};
+
+export const showNotification = async () => {
+  if (Notification.permission !== 'granted') {
+    sendLog('No permission to show notifications');
+    return;
+  }
+
+  try {
+    await self.registration.showNotification('Local Notification', {
+      body: 'This was sent from service worker, without using Push API',
+      icon: './icon-192.png',
+    });
+    sendLog('Local notification is sent successfully');
+  } catch (e) {
+    sendLog(`Error sending local notification: ${e}`);
+  }
+};
+
+export const requestPush = async (subscription: PushSubscription) => {
+  const result = await fetch('https://rockstar-push.glitch.me/send-push', {
+    method: 'post',
+    body: JSON.stringify(subscription, null, 2),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  const message = await result.text();
+  await sendLog(`Push request result: ${message}`);
+};
+
+/** END METHODS **/
 
 self.skipWaiting();
 clientsClaim();
@@ -23,7 +92,6 @@ self.addEventListener('activate', async () => {
   sendLog('Sevice worker is active');
   subscription = await registerPushSubscription();
 });
-
 
 self.addEventListener('message', (event) => {
   sendLog(`${event.data.type} event received by service worker`);
